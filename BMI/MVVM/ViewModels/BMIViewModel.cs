98392b8ea96c55a3fad59307debc54460b7c2771
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-02 09:33:14 +0330

    Add project files.

---
+++ BMI/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BMI.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public Models.BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new Models.BMI
+            {
+                Height = 180,
+                Weight = 55
+            };
+        }
+    }
+}
